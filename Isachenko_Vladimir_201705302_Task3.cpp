439cacf339016d4ba958f7c43abdc7e8461ea24a
Исаченко Владимир <vv.isachenko@gmail.com>
2017-04-17 20:10:04 +0700

    Added realization of distance tranform
    metric L0

--- Isachenko_Vladimir_201705302_Task3.cpp
+++ Isachenko_Vladimir_201705302_Task3.cpp
@@ -24,9 +24,73 @@ namespace mvms_2017
 		return LAST_NAME;
 	}
 
+	bool Isachenko_Vladimir_201705302_Task3::isOnBorder(cv::Mat img, int x, int y, int k)
+	{
+		int dx[] = { 0, 0, -1, 1 };
+		int dy[] = { 1, -1, 0, 0 };
+
+		for (int d = 0; d < 4; d++)
+		{
+			int curx = x + dx[d];
+			int cury = y + dy[d];
+
+			if (curx >= 0 && curx < img.cols && cury >= 0 && cury < img.rows)
+			{
+				int tmp = img.at<uchar>(cv::Point(curx, cury));
+				if (tmp == k)
+					return true;
+			}
+		}
+		return false;
+	}
+
 	cv::Mat Isachenko_Vladimir_201705302_Task3::distanceTransform(cv::Mat binimage)
 	{
-		return cv::Mat();
+		cv::Mat result = binimage.clone();
+
+		int H = binimage.cols, W = binimage.rows;
+
+		// get border
+		for (int i = 0; i < H; i++)
+		{
+			for (int j = 0; j < W; j++)
+			{
+				uchar cur = binimage.at<uchar>(cv::Point(i, j));
+				if (cur != 0)
+				{
+					bool flag = isOnBorder(binimage, i, j, 0);
+					if (i == 0 || i == H - 1 || j == 0 || j == W - 1 || flag)
+						result.at<uchar>(cv::Point(i, j)) = 1;
+					else
+						result.at<uchar>(cv::Point(i, j)) = 0;
+				}
+			}
+		}
+
+		int MAXITER = std::min(H, W);
+		for (int t = 1; t < MAXITER; t++)
+		{
+			bool haveChanges = false;
+			for (int i = 0; i < H; i++)
+			{
+				for (int j = 0; j < W; j++)
+				{
+					uchar cur = binimage.at<uchar>(cv::Point(i, j));
+					if (cur != 0 && result.at<uchar>(cv::Point(i, j)) == 0)
+					{
+						if (isOnBorder(result, i, j, t))
+						{
+							result.at<uchar>(cv::Point(i, j)) = std::min(t + 1, 255);
+							haveChanges = true;
+						}
+					}
+				}
+			}
+			if (!haveChanges)
+				break;
+		}
+
+		return result;
 	}
 
 }
--- Isachenko_Vladimir_201705302_Task3.h
+++ Isachenko_Vladimir_201705302_Task3.h
@@ -1,3 +1,4 @@
+﻿#pragma once
 #include "task3.h"
 
 namespace mvms_2017
@@ -6,6 +7,9 @@ namespace mvms_2017
 	{
 	private:
 		const int VARIANT_NUMBER = 201705302;
+		const std::string FIRST_NAME = "Владимир";
+		const std::string LAST_NAME = "Исаченко";
+		bool isOnBorder(cv::Mat img, int x, int y, int k);
 
 	public:
 		Isachenko_Vladimir_201705302_Task3();
@@ -15,6 +19,7 @@ namespace mvms_2017
 		std::string getFirstName() override;
 		std::string getSecondName() override;
 
+		//Реализовать алгоритм расчета расстояния до границы (Distance transform)
 		cv::Mat distanceTransform(cv::Mat binimage) override;
 	};
 }
