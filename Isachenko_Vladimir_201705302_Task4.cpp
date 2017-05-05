eb1d4408cb5ab31ab15678020118f0866a53f87b
Исаченко Владимир <vv.isachenko@gmail.com>
2017-05-05 22:54:47 +0700

    Updated algo #4
    updated Harris detector algo

--- Isachenko_Vladimir_201705302_Task4.cpp
+++ Isachenko_Vladimir_201705302_Task4.cpp
@@ -3,6 +3,9 @@
 
 namespace mvms_2017
 {
+	using cv::Mat;
+	using cv::Point;
+
 	Isachenko_Vladimir_201705302_Task4::Isachenko_Vladimir_201705302_Task4() : Task4(true)
 	{
 	}
@@ -24,8 +27,70 @@ namespace mvms_2017
 		return LAST_NAME;
 	}
 
-	cv::Mat Isachenko_Vladimir_201705302_Task4::Harris(cv::Mat grayimage, int window_size)
+	Mat Isachenko_Vladimir_201705302_Task4::Harris(Mat grayimage, int window_size)
 	{
-		return cv::Mat();
+		const float k = 0.04f;
+
+		// compute derivatives
+		int R = grayimage.rows, C = grayimage.cols;
+		Mat Ix = Mat(R, C, CV_8UC1);
+		Mat Iy = Mat(R, C, CV_8UC1);
+		cv::Sobel(grayimage, Ix, CV_8UC1, 1, 0);
+		cv::Sobel(grayimage, Iy, CV_8UC1, 0, 1);
+
+		// compute matrix M
+		int half_window = window_size / 2;
+		float norm = 1.0f / (window_size * window_size);
+		Mat no_thrsh = Mat(R, C, CV_32FC1);
+		for (int r = 0; r < R; r++)
+		{
+			for (int c = 0; c < C; c++)
+			{
+				float a11 = 0.f, a12 = 0.f, a21 = 0.f, a22 = 0.f;
+
+				for (int dx = -half_window; dx <= half_window; dx++)
+				{
+					for (int dy = -half_window; dy <= half_window; dy++)
+					{
+						int cr = r + dx, cc = c + dy;
+						if (cr < 0 || cc < 0 || cr >= R || cc >= C)
+							continue;
+
+						a11 += norm * Ix.at<uchar>(cv::Point(cc, cr)) * Ix.at<uchar>(cv::Point(cc, cr));
+						a22 += norm * Iy.at<uchar>(cv::Point(cc, cr)) * Iy.at<uchar>(cv::Point(cc, cr));
+						a21 += norm * Ix.at<uchar>(cv::Point(cc, cr)) * Iy.at<uchar>(cv::Point(cc, cr));
+						a12 += norm * Ix.at<uchar>(cv::Point(cc, cr)) * Iy.at<uchar>(cv::Point(cc, cr));
+					}
+				}
+
+				float det = a11*a22 - a12*a21;
+				float trace = a11 + a22;
+
+				no_thrsh.at<float>(cv::Point(c, r)) = (det - k * trace*trace);
+			}
+		}
+
+		Mat result = no_thrsh.clone();
+		for (int r = 0; r < R; r++)
+		{
+			for (int c = 0; c < C; c++)
+			{
+				for (int dx = -half_window; dx <= half_window; dx++)
+				{
+					for (int dy = -half_window; dy <= half_window; dy++)
+					{
+						int cr = r + dx, cc = c + dy;
+						if (cr < 0 || cc < 0 || cr >= R || cc >= C)
+							continue;
+						if (no_thrsh.at<float>(Point(c, r)) < no_thrsh.at<float>(Point(cc, cr)))
+						{
+							result.at<float>(Point(c, r)) = 0.f;
+						}
+					}
+				}
+			}
+		}
+
+		return result;
 	}
 }
